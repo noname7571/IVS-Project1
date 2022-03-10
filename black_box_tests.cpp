61611feadf79576909959e411b36e21313e267db
noname7571 <matej2808@gmail.com>
2022-03-10 16:26:20 +0100

    8/9 passed blackbox tests

--- black_box_tests.cpp
+++ black_box_tests.cpp
@@ -33,4 +33,116 @@
 //      *STEJNY* pocet cernych uzlu.
 //============================================================================//
 
+class EmptyTree : public ::testing::Test {
+    protected:
+        BinaryTree tree;
+};
+
+class NonEmptyTree : public ::testing::Test {
+    protected:
+        virtual void SetUp() {
+            int values[] = {10, 85, 15, 70, 20, 60, 30, 50, 65, 80, 90, 40, 5, 55};
+
+            for(int i = 0; i < 14; ++i) {
+                tree.InsertNode(values[i]);
+            }
+        }
+
+        BinaryTree tree;
+};
+
+class TreeAxioms : public ::testing::Test {
+    protected:
+        virtual void SetUp() {
+            int values[] = {10, 85, 15, 70, 20, 60, 30, 50, 65, 80, 90, 40, 5, 55};
+
+            for(int i = 0; i < 14; ++i) {
+                tree.InsertNode(values[i]);
+            }
+        }
+
+        BinaryTree tree;
+};
+
+TEST_F(EmptyTree, InsertNode) {
+    EXPECT_TRUE(tree.GetRoot() == NULL);
+
+    std::pair<bool, Node_t *> x = tree.InsertNode(0);
+    EXPECT_TRUE(x.first);
+    EXPECT_EQ(x.second, tree.GetRoot());
+
+    x = tree.InsertNode(0);
+    EXPECT_FALSE(x.first);
+    EXPECT_EQ(x.second, tree.GetRoot());
+}
+
+TEST_F(EmptyTree, DeleteNode) {
+    EXPECT_FALSE(tree.DeleteNode(0));
+    EXPECT_FALSE(tree.DeleteNode(25));
+
+    std::pair<bool, Node_t *> x = tree.InsertNode(0);
+    EXPECT_TRUE(tree.DeleteNode(0));
+}
+
+TEST_F(EmptyTree, FindNode) {
+    EXPECT_EQ(tree.FindNode(0), nullptr);
+    EXPECT_EQ(tree.FindNode(25), nullptr);
+
+    std::pair<bool, Node_t *> x = tree.InsertNode(0);
+    EXPECT_EQ(tree.FindNode(0)->key, 0);
+}
+
+TEST_F(NonEmptyTree, InsertNode) {
+    EXPECT_FALSE(tree.GetRoot() == NULL);
+
+    std::pair<bool, Node_t *> x = tree.InsertNode(0);
+    EXPECT_TRUE(x.first);
+    EXPECT_EQ(x.second, tree.FindNode(0));
+
+    x = tree.InsertNode(30);
+    EXPECT_FALSE(x.first);
+    EXPECT_EQ(x.second, tree.FindNode(30));
+}
+
+TEST_F(NonEmptyTree, DeleteNode) {
+    EXPECT_FALSE(tree.DeleteNode(0));
+    EXPECT_FALSE(tree.DeleteNode(25));
+
+    EXPECT_TRUE(tree.DeleteNode(70));
+    EXPECT_TRUE(tree.DeleteNode(55));
+}
+
+TEST_F(NonEmptyTree, FindNode) {
+    EXPECT_EQ(tree.FindNode(0), nullptr);
+    EXPECT_EQ(tree.FindNode(25), nullptr);
+
+    EXPECT_EQ(tree.FindNode(10)->key, 10);
+    EXPECT_EQ(tree.FindNode(40)->key, 40);
+}
+
+TEST_F(TreeAxioms, Axiom1) {
+    std::vector<BinaryTree::Node_t*> outLeafNodes;
+    tree.GetLeafNodes(outLeafNodes);
+
+    for (BinaryTree::Node_t* x : outLeafNodes) {
+        EXPECT_EQ(x->color, BLACK);
+    }
+}
+
+TEST_F(TreeAxioms, Axiom2) {
+    std::vector<BinaryTree::Node_t*> outAllNodes;
+    tree.GetAllNodes(outAllNodes);
+
+    for (BinaryTree::Node_t* x : outAllNodes) {
+        if (x->color == RED) {
+            EXPECT_EQ(x->pLeft->color, BLACK);
+            EXPECT_EQ(x->pRight->color, BLACK);
+        }
+    }
+}
+
+TEST_F(TreeAxioms, Axiom3) {
+    
+}
+
 /*** Konec souboru black_box_tests.cpp ***/
